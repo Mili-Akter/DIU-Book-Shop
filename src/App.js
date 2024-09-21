78e99d525cb67569dfbaad0ffb098e4e6c5622e5
Mili-Akter <miliakter6078@gmail.com>
2024-09-21 19:27:27 +0600

    add home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Category from './components/Home/Category';
+import Delivery from './components/Home/Delivery';
+import Featured from './components/Home/Featured';
+import Hero from './components/Home/Hero';
+import Testimonials from './components/Home/Testimonials';
+import Footer from './components/Layout/Footer';
+import Navbar from './components/Layout/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <heder className='relative overflow-hidden'>
+        <Navbar />
+        <Hero />
+      </heder>
+      <main>
+        <Category />
+        <div className='relative overflow-hidden'>
+          {/* Decorative background image and gradient */}
+          <div aria-hidden='true' className='absolute inset-0'>
+            <div className='absolute inset-0 mx-auto max-w-7xl overflow-hidden xl:px-8'>
+              <img
+                src='https://tailwindui.com/img/ecommerce-images/home-page-02-sale-full-width.jpg'
+                alt=''
+                className='h-full w-full object-cover object-center'
+              />
+            </div>
+            <div className='absolute inset-0 bg-white bg-opacity-75' />
+            <div className='absolute inset-0 bg-gradient-to-t from-white via-white' />
+          </div>
+          <Featured />
+          <Testimonials />
+        </div>
+        <Delivery />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
-}
+};
 
 export default App;
